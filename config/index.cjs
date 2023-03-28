ca5973ee7bd5b4ece21884f2870d38c6ea123f1f
1366928952 <51068548+1366928952@users.noreply.github.com>
2023-03-28 11:19:53 +0800

    Update index.cjs
    和海军

--- config/index.cjs
+++ config/index.cjs
@@ -29,14 +29,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '丁家宝', year: '1998', date: '07-11',
+          type: '*生日', name: '丁家宝', year: '1998', date: '05-18',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
 //         {
 //           type: '生日', name: '李四', year: '1996', date: '09-31',
-//         },
-//         {
-//           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
 //         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -51,7 +48,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'RPjY-Zzq8Ieu_Q3L2TP6mqVU7NEqnvWPzhxbArL6Llg	',
+  CALLBACK_TEMPLATE_ID: 'RPjY-Zzq8Ieu_Q3L2TP6mqVU7NEqnvWPzhxbArL6Llg',
 
   CALLBACK_USERS: [
     {
